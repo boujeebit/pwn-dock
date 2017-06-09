87a9a85fa50dfbbd334f03dc7c6eecaa38a93347
mhefley@github.com <hefleytech@gmail.com>
2017-06-09 16:48:35 -0500

    bashrc changes

--- metasploit/scripts/bashrc.sh
+++ metasploit/scripts/bashrc.sh
@@ -4,5 +4,5 @@ service postgresql start
 su -c "mkdir /var/lib/postgresql/.msf4 && createuser msf && createdb --owner=msf msf && msfdb init" postgres
 mkdir /root/.msf4/
 cp /var/lib/postgresql/.msf4/database.yml /root/.msf4/database.yml
-echo service postgresql start >> /root/.bashrc
-echo su -c "msfdb init" postgres >> /root/.bashrc
+echo service postgresql start > /root/.bashrc
+echo su -c \"msfdb init\" postgres >> /root/.bashrc
